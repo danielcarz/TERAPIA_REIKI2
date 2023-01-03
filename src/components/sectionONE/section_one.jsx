43714fc81e0d179662a2729041e58a6f47f40887
danielcarz <dcz9412@gmail.com>
2023-01-03 11:01:14 -0500

    ASSETS: agregamos imagenes

--- src/components/sectionONE/section_one.jsx
+++ src/components/sectionONE/section_one.jsx
@@ -3,12 +3,15 @@ import React from "react";
 //SASS
 import '../../styles/_section_one.scss'
 
+//IMAGES
+import logo from '../../assets/LOGO-ASHIRA-2020.png'
+
 const Section_one = () => {
 
     return(
         <header className="section section_one">
             <nav className="navbar">
-                <figure className="imagen_logo"><img src="" alt="logo" /></figure>
+                <figure className="imagen_logo"><img src={logo} alt="logo" /></figure>
                 <ul className="ulist">
                     <li><a href="">section 1</a></li>
                     <li><a href="">section 1</a></li>
@@ -18,7 +21,7 @@ const Section_one = () => {
                     
                 </ul>
 
-                <div><button>reserva</button></div>
+                <div><button><a href="">RESERVA</a></button></div>
             </nav>
 
             <figure className="imagen_servicio"><img src="" alt="imagen_servicio" /></figure>
