7c7bd53fe1803fd55d7ecb3494395f88fc830bcb
danielcarz <dcz9412@gmail.com>
2023-01-05 17:53:40 -0500

    commit

--- src/components/sectionTWO/section_two.jsx
+++ src/components/sectionTWO/section_two.jsx
@@ -6,8 +6,11 @@ import '../../styles/_section_two.scss'
 const Section_two = () => {
 
     return(
-        <section className="section">
-            <h1 className="five">SECTION TWO</h1>
+        <section className="section section_two">
+            <section className="introducion">
+                <h2></h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias consequatur magnam, nihil quasi nemo pariatur non, id nesciunt sint eveniet cupiditate! Dolores nemo enim placeat quisquam veniam. Itaque laudantium quidem, minima quo obcaecati maiores ut provident quaerat incidunt expedita! Optio error autem cupiditate aliquid. Beatae nam maiores commodi rem laudantium natus iusto id veniam vitae. Nesciunt assumenda unde fugiat saepe, deleniti fuga ullam ad id recusandae adipisci provident beatae, repudiandae laborum libero inventore consequatur maiores, eveniet iure quas suscipit aliquid! Laudantium ab animi voluptas, eligendi libero voluptatibus quas inventore quasi laboriosam quibusdam. Rerum animi fugiat ab quaerat enim omnis sed?</p>
+            </section>
         </section>
     )
 }
