d60665530f43822241318ad46a4236fb1101b431
danielcarz <dcz9412@gmail.com>
2023-01-06 09:18:23 -0500

    SECTION ONE: cambio imagen de portada

--- src/components/sectionTHREE/section_three.jsx
+++ src/components/sectionTHREE/section_three.jsx
@@ -3,12 +3,87 @@ import React from "react";
 //SASS
 import '../../styles/_section_three.scss'
 
+//IMAGEN
+import masaje_relajante from '../../assets/Masaje-relajante.jpg'
+
 const Section_three = () => {
 
     return(
 
         <section className="section">
-            <h1 className="five">SECTION THREE</h1>
+
+            <section className="servicios_wrapper">
+                <figure className="imagen_servicios"><img src={masaje_relajante} alt="servicios" /></figure>
+                <section className="servicios_introduccion">
+                    <h3>servicio1</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor eos alias fugit sint atque aperiam commodi quidem explicabo. Voluptates, labore.</p>
+                </section>
+
+            </section>
+
+            <section className="servicios_wrapper">
+                <figure className="imagen_servicios"><img src="" alt="servicios" /></figure>
+                <section className="servicios_introduccion">
+                    <h3>servicio1</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor eos alias fugit sint atque aperiam commodi quidem explicabo. Voluptates, labore.</p>
+                </section>
+
+            </section>
+
+            <section className="servicios_wrapper">
+                <figure className="imagen_servicios"><img src="" alt="servicios" /></figure>
+                <section className="servicios_introduccion">
+                    <h3>servicio1</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor eos alias fugit sint atque aperiam commodi quidem explicabo. Voluptates, labore.</p>
+                </section>
+
+            </section>
+
+            <section className="servicios_wrapper">
+                <figure className="imagen_servicios"><img src="" alt="servicios" /></figure>
+                <section className="servicios_introduccion">
+                    <h3>servicio1</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor eos alias fugit sint atque aperiam commodi quidem explicabo. Voluptates, labore.</p>
+                </section>
+
+            </section>
+
+            <section className="servicios_wrapper">
+                <figure className="imagen_servicios"><img src="" alt="servicios" /></figure>
+                <section className="servicios_introduccion">
+                    <h3>servicio1</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor eos alias fugit sint atque aperiam commodi quidem explicabo. Voluptates, labore.</p>
+                </section>
+
+            </section>
+
+            <section className="servicios_wrapper">
+                <figure className="imagen_servicios"><img src="" alt="servicios" /></figure>
+                <section className="servicios_introduccion">
+                    <h3>servicio1</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor eos alias fugit sint atque aperiam commodi quidem explicabo. Voluptates, labore.</p>
+                </section>
+
+            </section>
+
+            <section className="servicios_wrapper">
+                <figure className="imagen_servicios"><img src="" alt="servicios" /></figure>
+                <section className="servicios_introduccion">
+                    <h3>servicio1</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor eos alias fugit sint atque aperiam commodi quidem explicabo. Voluptates, labore.</p>
+                </section>
+
+            </section>
+
+            <section className="servicios_wrapper">
+                <figure className="imagen_servicios"><img src="" alt="servicios" /></figure>
+                <section className="servicios_introduccion">
+                    <h3>servicio1</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor eos alias fugit sint atque aperiam commodi quidem explicabo. Voluptates, labore.</p>
+                </section>
+
+            </section>
+            
         </section>
     )
 }
